e139cc26b46d3bfdcb7d1343223833f99fd982d1
Lanelle64 <77619855+Lanelle64@users.noreply.github.com>
2024-03-24 16:40:01 +0100

    Final - fixed message

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -63,25 +63,42 @@ export async function node(
             newX = "1";
           }
           for (let i = 0; i < N; i++) {
-            if (!messagesP.has(k)) {
-              messagesP.set(k, []);
-            }
-            messagesP.get(k)!.push(newX);
+            axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, {k: k, x: newX, messageType: "P"});
           }
-          res.status(200).send("ok");
-        } else {
-          res.status(200).send("ok");
         }
       } else if (messageType == "P") {
         if (!messagesP.has(k)) {
           messagesP.set(k, []);
         }
         messagesP.get(k)!.push(x);
-        res.status(200).send("ok");
+        let messageP = messagesP.get(k)!;
+        if (messageP.length >= N - F) {
+          let count0 = messageP.filter((el) => el == 0).length;
+          let count1 = messageP.filter((el) => el == 1).length;
+
+          if (count0 >= F + 1) {
+            nodeState.x = 0;
+            nodeState.decided = true;
+          } else if (count1 >= F + 1) {
+            nodeState.x = 1;
+            nodeState.decided = true;
+          } else {
+            if (count0 + count1 > 0 && count0 > count1) {
+              nodeState.x = 0;
+            } else if (count0 + count1 > 0 && count0 < count1) {
+              nodeState.x = 1;
+            } else {
+              nodeState.x = Math.random() > 0.5 ? 0 : 1;
+            }
+            nodeState.k = k + 1;
+            for (let i = 0; i < N; i++) {
+              axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, {k: k + 1, x: nodeState.x, messageType: "R"});
+            }
+          }
+        }
       }
-    } else {
-      res.status(500).send("faulty");
     }
+    res.status(200).send("message received");
   });
 
   // TODO implement this
